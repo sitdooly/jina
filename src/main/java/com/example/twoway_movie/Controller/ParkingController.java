3f28f461f33fb66c6ca7216f4acbd7465420cb62
OckHwanKim <sitdooly@naver.com>
2026-01-13 16:39:42 +0900

    2조로그인화일22

---
+++ src/main/java/com/example/twoway_movie/Controller/ParkingController.java
@@ -0,0 +1,29 @@
+package com.example.twoway_movie.Controller;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+@RestController
+//@RequestMapping("/parking")
+public class ParkingController {
+
+
+    @GetMapping("/search")
+    public String kk1(){
+        return"parking";
+    }
+
+}
